fe84924b5f23cfdd455c9ab6302bbea84cd828b3
GabrielPCarvalho <gabriel57_pereira@hotmail.com>
2022-05-02 20:31:28 -0300

    create card

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -5,7 +5,7 @@ export default {
         },
         secondary: {
             main:'#FF872C',
-            light: 'rgba(255, 135, 44, 0.3)'
+            light: 'rgba(255, 135, 44, 0.3)',
         },
         success: {
             main:'#12A454',
@@ -18,6 +18,7 @@ export default {
         shape: '#FFFFFF',
         title: '#363F5F',
         text: '#969CB2',
+        text_dark: '#000000',
         background: '#F0F2F5',
     },
 
@@ -25,5 +26,5 @@ export default {
         regular: 'Poppins_400Regular',
         medium: 'Poppins_500Medium',
         bold: 'Poppins_700Bold',
-    }
+    },
 }
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,46 @@
 import React from "react";
-
-import { Container, Title } from "./styles";
+import { HighlightCard } from "../../components/HighlightCard";
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards
+ } from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/81686496?v=4'}}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Gabriel</UserName>
+                        </User>
+                    </UserInfo>
+                           
+                <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+
+            <HighlightCards
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingHorizontal: 24 }}
+            >
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
         </Container>
     );
 }
